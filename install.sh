00bb0c6a183e88f88059a43cfb08e35bc0accea7
Patrick Rhodes Martin <patrick.martin.r@gmail.com>
2023-02-17 14:07:54 -0800

    Minor formatting fixes.

--- install.sh
+++ install.sh
@@ -18,8 +18,10 @@ mkdir gamescope
 pushd gamescope
 wget https://builds.garudalinux.org/repos/chaotic-aur/x86_64/gamescope-git-$GAMESCOPE_VERSION-x86_64.pkg.tar.zst >/dev/null
 sudo tar --use-compress-program=unzstd -xvf ./gamescope-git-$GAMESCOPE_VERSION-x86_64.pkg.tar.zst >/dev/null
+echo
 echo "Installing gamescope update..."
 sudo rsync -a ./usr /opt/rwfus/mount/upper/ >/dev/null
+echo
 echo "Creating uninstall script..."
 sudo sh -c 'echo "#!/bin/sh" > /opt/rwfus/mount/upper/usr/bin/hdrdeck_uninstall'
 sudo sh -c 'echo "echo Uninstalling HDRDeck..." >> /opt/rwfus/mount/upper/usr/bin/hdrdeck_uninstall'
@@ -32,8 +34,10 @@ sudo sh -c 'echo "read -p \"Please press enter to reboot.\"" >> /opt/rwfus/mount
 sudo sh -c 'echo "sudo reboot" >> /opt/rwfus/mount/upper/usr/bin/hdrdeck_uninstall'
 sudo chmod 555 /opt/rwfus/mount/upper/usr/bin/hdrdeck_uninstall
 popd >/dev/null
+echo
 echo "Cleaning up our extracted gamescope files..."
 sudo rm -rf ./gamescope
+echo
 echo "Enabling HDR..."
 if grep -q 'DXVK_HDR' ~/.bash_profile; then
     echo "export ENABLE_GAMESCOPE_WSI=1" >> .bash_profile
@@ -41,7 +45,8 @@ if grep -q 'DXVK_HDR' ~/.bash_profile; then
 fi
 sudo cp /usr/bin/gamescope-session /opt/rwfus/mount/upper/usr/bin/gamescope-session
 sudo sed -i 's/gamescope \\/gamescope --hdr-enabled \\/' /opt/rwfus/mount/upper/usr/bin/gamescope-session
-popd
+popd >/dev/null
+echo
 echo "To uninstall please run the command \"hdrdeck_uninstall\""
 read -p "Please press enter to reboot."
 sudo reboot
